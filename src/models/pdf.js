3dc62aec749075d59e55adbdfb58dcbe7cb462f0
palla raghavendra <palla.raghavendra@freecharge.com>
2025-07-20 22:04:47 +0530

    added hello js file

--- src/models/pdf.js
+++ src/models/pdf.js
@@ -1,20 +1,26 @@
 import ejs from 'ejs'
 import puppeteer from 'puppeteer'
 
-async function getPdfBase64(body, params) {
+async function getPdfBase64(body) {
     const { template = '', data = {}} = body
-    const renderedHtml = ejs.render(template, data);
+    try{
+        const renderedHtml = ejs.render(template, data);
 
-    // Generate PDF from rendered HTML
-    const browser = await puppeteer.launch({ headless: 'new' });
-    const page = await browser.newPage();
-    await page.setContent(renderedHtml, { waitUntil: 'networkidle0' });
-    const pdfBuffer = await page.pdf({ format: 'A4' });
-    await browser.close();
-
-    // Convert to base64
-    const base64Pdf = pdfBuffer.toString('base64');
-    return {base64: base64Pdf}
+        // Generate PDF from rendered HTML
+        const browser = await puppeteer.launch({ headless: 'new' });
+        const page = await browser.newPage();
+        await page.setContent(renderedHtml, { waitUntil: 'networkidle0' });
+        const pdfBuffer = await page.pdf({ format: 'A4' });
+        await browser.close();
+    
+        // Convert to base64
+        const base64Pdf = pdfBuffer.toString('base64');
+        return {base64: base64Pdf}
+    }
+    catch(error){
+        throw new Error(`Failed to generate PDF: ${error.message}`)
+    }
+    
 
 }
 
